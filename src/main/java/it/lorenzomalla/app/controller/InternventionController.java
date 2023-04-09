c5234d7ee76ee3a43ac9349588c24d0d00573410
Lorenzo Mallardo <lorenzomallardo@MBP-DI-LORENZO.station>
2023-04-09 15:40:00 +0200

    add context base-path, modify swagger and add intervention service

--- src/main/java/it/lorenzomalla/app/controller/InternventionController.java
+++ src/main/java/it/lorenzomalla/app/controller/InternventionController.java
@@ -2,6 +2,7 @@ package it.lorenzomalla.app.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,18 +20,18 @@ public class InternventionController implements InterventionsApi {
 	@Override
 	public ResponseEntity<Intervention> createNewIntervention(String customerId, String vehicleId,
 			@Valid Intervention intervention) {
-		// TODO Auto-generated method stub
-		return InterventionsApi.super.createNewIntervention(customerId, vehicleId, intervention);
+		return ResponseEntity.status(HttpStatus.CREATED).body(interventionService.createNewIntervention(customerId, vehicleId, intervention));
 	}
 
 	@Override
 	public ResponseEntity<Void> deleteIntervention(String interventionId) {
-		return InterventionsApi.super.deleteIntervention(interventionId);
+		interventionService.deleteIntervention(interventionId);
+		return ResponseEntity.ok().build();
 	}
 
 	@Override
 	public ResponseEntity<Intervention> updateIntervention(String interventionId, @Valid Intervention intervention) {
-		return InterventionsApi.super.updateIntervention(interventionId, intervention);
+		return ResponseEntity.ok().body(interventionService.updateIntervention(interventionId, intervention));
 	}
 
 }
